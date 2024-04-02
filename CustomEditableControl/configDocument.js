b37bf01afd6f2287f4d6af7c9d1fb767d04726b6
daniel.li@artsolutions.io <daniel.li@artsolutions.io>
2024-04-02 14:29:29 +0800

    CustomEditableControl 修正規範

--- CustomEditableControl/configDocument.js
+++ CustomEditableControl/configDocument.js
@@ -2,26 +2,18 @@ sample =
     [
         {
             "code": "string", // unique for each rule tree
+            "listType": "string", // options: ["blacklist", "whitelist"]
             "rulePath": [
                 {
                     "nodeCode": "string", // unique for each rule tree node. "root" for first node.
                     "checker": "string", // options: ["bpfstage", "role", "team", "status", ...] *... for extension package codes
-                    "checkerType": "string", // options :["blacklist", "whitelist", "switch"]
+                    "checkerType": "string", // options :["list", "switch"]
                     "rules": [
-                        // for "switch" checkerType
-                        // {case, nextNode, result}
+                        // {case, nextNode, includeControl}
                         {
-                            "case": "string",
+                            "case": "string", // string for "switch" checkerType; string[] for "list" checkerType; null for unmatched checker
                             "nextNode": "string", // null if no next node, then return result
-                            "result": "string", // null if there's next node
-                        },
-
-                        // for "blacklist" and "whitelist"
-                        // {list, nextNode, result} 
-                        {
-                            "list": [], // string array
-                            "nextNode": "string", // null if no next node, then return result
-                            "result": "string", // null if there's next node
+                            "includeControl": [], // to add in list
                         }
                     ]
                 }
--- CustomEditableControl/configDocument.ts
+++ CustomEditableControl/configDocument.ts
@@ -3,36 +3,25 @@ let EnvJson: Config[];
 class Config {
     // code: unique;
     code: string;
+    listType: "blacklist" | "whitelist";
     rulePath: RulePathNode[];
 }
 
 class RulePathNode {
-    // nodeCode: exactly one RulePathNode shall named "root"
-    // nodeCode: shall be unique
+    // nodeCode: exactly one RulePathNode shall named "root", shall be unique
     // checker: string for custom checker such as attribute, flag or user
     // rules: shall not mix different node rules
-    nodeCode: string;
+    nodeCode: "root" | string;
     checker: "bpfstage" | "role" | "team" | "status" | string;
     checkerType: "list" | "switch";
     rules: NodeRule[];
 }
 
 class NodeRule {
-    // result: "__all__" for all controls
+    // case: string for "switch" checkerType; string[] for "list" checkerType; null for unmatched checker
+    // nextNode: set null to stop rule path
+    // includeControl: "__all__" for all controls
+    case: string | string[] | null;
     nextNode: string | null;
-    result: "__all__" | string[];
-}
-
-class CaseNodeRule extends NodeRule {
-    // case: null for unmatched checker
-    case: string | null;
-}
-
-class ListNodeRule extends NodeRule {
-    // list: null for unmatched checker
-    list: string[] | null;
-}
-
-class CustomRule extends NodeRule {
-    // *add custom determine rule if needed.
+    includeControl: "__all__" | string[];
 }
