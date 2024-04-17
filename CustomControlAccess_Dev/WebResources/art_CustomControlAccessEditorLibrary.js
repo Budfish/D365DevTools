0d9e7472bb9fa22c30069d2309d8f12f9ad9a1e8
Budfish <87319891+Budfish@users.noreply.github.com>
2024-04-18 01:41:35 +0800

    建立模版及CSS

--- CustomControlAccess_Dev/WebResources/art_CustomControlAccessEditorLibrary.js
+++ CustomControlAccess_Dev/WebResources/art_CustomControlAccessEditorLibrary.js
@@ -5,31 +5,128 @@ class RuleNode {
 
     }
     _constructDom() {
-        this._ruleNode = document.createElement("div");
-        this._nodeCode = document.createElement("input");
-        this._checker = document.createElement("input");
-        this._checkerType = document.createElement("input");
-        this._rulesContainer = document.createElement("div");
-        setupRuleNode();
-        setupNodeCode();
-        setupChecker();
-        setupRulesContainer();
+        const node = document.createElement("div");
+        const head = getHead();
+        const body = getBody();
+        node.classList.add("cca_ruleNode");
+        node.classList.add("cca_card");
+        node.appendChild(head);
+        node.appendChild(body);
 
-        function setupRuleNode() {
+        function getHead() {
+            const node = document.createElement("div");
+            const heading = getHeading();
+            const tools = getTools();
+            node.classList.add("cca_head");
+            node.classList.add("cca_row");
+            node.appendChild(heading);
+            node.appendChild(tools);
+            return node;
 
+            function getHeading() {
+                const node = document.createElement("div");
+                node.textContent = "heading";
+                return node;
+            }
+            function getTools() {
+                const node = document.createElement("div");
+                node.textContent = "tools";
+                return node;
+            }
         }
-        function setupNodeCode() {
+        function getBody() {
+            const node = document.createElement("div");
+            const codeLine = getCodeLine();
+            const checkerLine = getCheckerLine();
+            const rulesArea = getRulesArea();
+            node.classList.add("cca_body");
+            node.appendChild(codeLine);
+            node.appendChild(checkerLine);
+            node.appendChild(rulesArea);
+            return node;
 
-        }
-        function setupChecker() {
+            function getCodeLine() {
+                const node = document.createElement("div");
+                const label = getLabel();
+                const input = getInput();
+                node.classList.add("cca_row");
+                node.appendChild(label);
+                node.appendChild(input);
+                return node;
 
-        }
-        function setupRulesContainer() {
+                function getLabel() {
+                    const node = document.createElement("div");
+                    const context = getContext();
+                    node.classList.add("cca_label");
+                    node.appendChild(context);
+                    return node;
 
+                    function getContext() {
+                        const node = document.createElement("span");
+                        node.textContent = "code";
+                        return node;
+                    }
+                }
+                function getInput() {
+                    const node = document.createElement("input");
+                    const input = getInput();
+                    node.classList.add("cca_fill");
+                    node.appendChild(input)
+                    return node;
+
+                    function getInput() {
+                        const node = document.createElement("input");
+                        node.type = "text";
+                        return node;
+                    }
+                }
+            }
+            function getCheckerLine() {
+                const node = document.createElement("div");
+                const label = getLabel();
+                const input = getInput();
+                node.classList.add("cca_row");
+                node.appendChild(label);
+                node.appendChild(input);
+                return node;
+
+                function getLabel() {
+                    const node = document.createElement("div");
+                    const context = getContext();
+                    node.classList.add("cca_label");
+                    node.appendChild(context);
+                    return node;
+
+                    function getContext() {
+                        const node = document.createElement("span");
+                        node.textContent = "checker";
+                        return node;
+                    }
+                }
+                function getInput() {
+                    const node = document.createElement("input");
+                    const input = getInput();
+                    node.classList.add("cca_fill");
+                    node.appendChild(input)
+                    return node;
+
+                    function getInput() {
+                        const node = document.createElement("input");
+                        node.type = "text";
+                        return node;
+                    }
+                }
+            }
+            function getRulesArea() {
+                const node = document.createElement("div");
+                return node;
+
+                // TODO
+            }
         }
     }
 }
-class NodeRule {
+class Rule {
     index;
     parent;
     constructor() {
