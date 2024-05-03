b900b4d3247b2e8bcdbba327e186ab4ba58f854b
AzureAD\AdonisMa <Adonis.ma@artsolutions.io>
2024-05-03 17:35:15 +0800

    [ART]  BuildCaseCuctomization - BuildCaseDataSyncPlugins - 新增 3 個 model 的內容值

--- BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/Models/ManagerSyncModel.cs
+++ BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/Models/ManagerSyncModel.cs
@@ -8,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace BuildCaseDataSyncPlugins.EntitySyncService.Models
 {
-    internal class ManagerSyncModel
+    public class ManagerSyncModel
     {
+        public string result { get; set; }
+        public string message { get; set; }
+        public DataResult[] dataResult { get; set; }
+
+        public class DataResult
+        {
+            public string empID { get; set; }
+            public string empTwName { get; set; }
+            public string empCorpOrgID { get; set; }
+            public string corpOrgName { get; set; }
+            public string corpOrgIdSeq { get; set; }
+            public string statusMessage { get; set; }
+            public string corpOrgNameSeq { get; set; }
+        }
     }
 }
--- BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/Models/OrganizationSyncModel.cs
+++ BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/Models/OrganizationSyncModel.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace BuildCaseDataSyncPlugins.EntitySyncService.Models
 {
-    internal class OrganizationSyncModel
+    public class OrganizationSyncModel
     {
+        public string result { get; set; }
+        public string message { get; set; }
+        public DataResult[] dataResult { get; set; }
+
+        public class DataResult
+        {
+            public string orgType { get; set; }
+            public string authType { get; set; }
+            public string corpOrgID { get; set; }
+            public string corpOrgTwName { get; set; }
+            public string lev { get; set; }
+            public string sysLevel { get; set; }
+            public string corpRegionCd { get; set; }
+            public string managerEmpID { get; set; }
+            public string managerEmpTwName { get; set; }
+            public string deptID { get; set; }
+            public string deptName { get; set; }
+            public string divID { get; set; }
+            public string divName { get; set; }
+            public string unitID { get; set; }
+            public string unitName { get; set; }
+            public string parentCorpOrgID { get; set; }
+            public string parentCorpOrgTwName { get; set; }
+            public string idSeq { get; set; }
+            public string nameSeq { get; set; }
+            public string corpOrgCatgCd { get; set; }
+            public string misTeamno { get; set; }
+            public string isMisCreate { get; set; }
+        }
     }
 }
