467dea38e0e653fa030ded409b009743f63feae8
daniel.li@artsolutions.io <daniel.li@artsolutions.io>
2024-05-03 17:11:03 +0800

    建立EntitySyncServiceBase邏輯與ManagerSyncService範本

--- BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/EntitySyncServiceBase.cs
+++ BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/EntitySyncServiceBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,38 +12,70 @@ namespace BuildCaseDataSyncPlugins.EntitySyncService
 {
     internal abstract class EntitySyncServiceBase
     {
-        private IOrganizationService service;
-        private IExecutionContext context;
-        private ITracingService tracer;
-        protected abstract string syncApiAction { get; set; }
-        protected abstract string entityLogicalName { get; set; }
-        protected abstract string entityKeyName { get; set; }
-        protected abstract Type modelType { get; set; }
-        protected abstract Dictionary<string, string> fieldMap { get; set; }
+        protected IOrganizationService service;
+        protected IExecutionContext context;
+        protected ITracingService tracer;
+        protected abstract string syncApiAction { get; }
+        protected abstract string syncApiPostBody { get; }
+        protected abstract string entityLogicalName { get; }
+        protected abstract string entityKeyName { get; }
+        protected abstract Type modelType { get; }
         public EntitySyncServiceBase(IExecutionContext context, IOrganizationService service, ITracingService tracer)
         {
             this.service = service;
             this.tracer = tracer;
             this.context = context;
         }
+        internal abstract bool CallApiGetData(out string resultStr);
+        internal abstract Entity ConvertD365EntityData<T>(T apiData);
         internal void RetrieveDataAndSync<T>()
         {
-            T[] result = JsonSerializer.Deserialize<T[]>("");
+            bool apiSuccess = CallApiGetData(out string resultStr);
+            if(!apiSuccess)
+            {
+                tracer.Trace("[Error] CallApiGetData fails.");
+                tracer.Trace(resultStr);
+                return;
+            }
+
+            T[] result;
+            try
+            {
+                result = JsonSerializer.Deserialize<T[]>(resultStr);
+            }
+            catch (Exception)
+            {
+                tracer.Trace("[Error] JsonDeserialize fails.");
+                tracer.Trace(resultStr);
+                return;
+            }
+
             foreach (T item in result)
             {
                 Entity entityData = ConvertD365EntityData(item);
                 UpsertRecord(entityData);
             }
         }
-        internal bool CallApiGetData(out string resultStr)
+        private void UpsertRecord(Entity entity)
         {
-            resultStr = "";
-            return true;
-        }
-        internal abstract Entity ConvertD365EntityData<T>(T apiData);
-        internal void UpsertRecord(Entity entity)
-        {
+            QueryExpression query = new QueryExpression()
+            {
+                EntityName = entityLogicalName,
+                Criteria = new FilterExpression(),
+            };
+            query.Criteria.AddCondition(entityKeyName, ConditionOperator.Equal, entity[entityKeyName]);
+            EntityCollection existingRecord = service.RetrieveMultiple(query);
 
+            if (existingRecord.Entities.Count == 0)
+            {
+                service.Create(entity);
+            }
+            else
+            {
+                Entity updateRecord = existingRecord.Entities.First();
+                entity.Id = updateRecord.Id;
+                service.Update(entity);
+            }
         }
     }
 }
--- BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/Services/ManagerSyncService.cs
+++ BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/Services/ManagerSyncService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BuildCaseDataSyncPlugins.EntitySyncService.Models;
+using Microsoft.Xrm.Sdk;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace BuildCaseDataSyncPlugins.EntitySyncService.Services
 {
-    internal class ManagerSyncService
+    internal class ManagerSyncService : EntitySyncServiceBase
     {
+        public ManagerSyncService(IExecutionContext context, IOrganizationService service, ITracingService tracer) : base(context, service, tracer) { }
+        protected override string syncApiAction => "";
+        protected override string syncApiPostBody => "";
+        protected override string entityLogicalName => "";
+        protected override string entityKeyName => "";
+        protected override Type modelType => typeof(ManagerSyncModel);
+
+        internal override bool CallApiGetData(out string resultStr)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override Entity ConvertD365EntityData<T>(T apiData)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
