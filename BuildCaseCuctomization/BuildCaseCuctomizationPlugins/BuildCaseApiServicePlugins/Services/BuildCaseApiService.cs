5e5a17b8d133c4f27b6a4828100e023f3a6571c1
daniel.li@artsolutions.io <daniel.li@artsolutions.io>
2024-05-03 15:22:27 +0800

    初始化Plugins

---
+++ BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseApiServicePlugins/Services/BuildCaseApiService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildCaseApiServicePlugins.Services
+{
+    internal class BuildCaseApiService
+    {
+        private IOrganizationService service;
+        private IExecutionContext context;
+        private ITracingService tracer;
+        private string domain { get; set; }
+        public BuildCaseApiService(IExecutionContext context, IOrganizationService service, ITracingService tracer)
+        {
+            this.service = service;
+            this.tracer = tracer;
+            this.context = context;
+
+            domain = GetDomain();
+        }
+        private string GetDomain()
+        {
+            var request = new OrganizationRequest("RetrieveEnvironmentVariableValue");
+            request.Parameters["DefinitionSchemaName"] = "art_BuildCaseApiEndPoint";
+            var response = service.Execute(request);
+            var value = response.Results["Value"];
+            return value != null ? value.ToString() : "";
+        }
+        private string GetAccessToken()
+        {
+            return "";
+        }
+        private string GetApiRoute(string action)
+        {
+            return domain + action;
+        }
+        internal string GetResponseString(string action, string requestBodyJson)
+        {
+            string apiRoute = GetApiRoute(action);
+            string accessToken = GetAccessToken();
+            try
+            {
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        client.Timeout = TimeSpan.FromMilliseconds(15000);
+                        client.DefaultRequestHeaders.ConnectionClose = true;
+                        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+
+                        HttpContent postBody = new StringContent(requestBodyJson, Encoding.UTF8, "application/json");
+                        HttpResponseMessage response = client.PostAsync(apiRoute, postBody).Result;
+                        response.EnsureSuccessStatusCode();
+
+                        string responseText = response.Content.ReadAsStringAsync().Result;
+                        return responseText;
+                    }
+                }
+                #region catch AggregateException
+                catch (AggregateException aex)
+                {
+                    tracer.Trace("Inner Exceptions:");
+                    foreach (Exception ex in aex.InnerExceptions)
+                    {
+                        tracer.Trace("  Exception: {0}", ex.ToString());
+                    }
+                    string errorMessage = string.Format(CultureInfo.InvariantCulture, "An exception occurred while attempting to issue the request.", aex);
+                    throw new InvalidPluginExecutionException(errorMessage);
+                }
+                #endregion
+            }
+            #region catch Exception
+            catch (Exception e)
+            {
+                tracer.Trace("Exception: {0}", e.ToString());
+            }
+            #endregion
+            return $"Api: {action} 呼叫失敗。";
+        }
+    }
+}
---
+++ BuildCaseCuctomization/BuildCaseCuctomizationPlugins/BuildCaseDataSyncPlugins/EntitySyncService/EntitySyncServiceBase.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BuildCaseDataSyncPlugins.EntitySyncService
+{
+    internal abstract class EntitySyncServiceBase
+    {
+        private IOrganizationService service;
+        private IExecutionContext context;
+        private ITracingService tracer;
+        protected abstract string syncApiAction { get; set; }
+        protected abstract string entityLogicalName { get; set; }
+        protected abstract string entityKeyName { get; set; }
+        protected abstract Type modelType { get; set; }
+        protected abstract Dictionary<string, string> fieldMap { get; set; }
+        public EntitySyncServiceBase(IExecutionContext context, IOrganizationService service, ITracingService tracer)
+        {
+            this.service = service;
+            this.tracer = tracer;
+            this.context = context;
+        }
+        internal void RetrieveDataAndSync<T>()
+        {
+            T[] result = JsonSerializer.Deserialize<T[]>("");
+            foreach (T item in result)
+            {
+                Entity entityData = ConvertD365EntityData(item);
+                UpsertRecord(entityData);
+            }
+        }
+        internal bool CallApiGetData(out string resultStr)
+        {
+            resultStr = "";
+            return true;
+        }
+        internal abstract Entity ConvertD365EntityData<T>(T apiData);
+        internal void UpsertRecord(Entity entity)
+        {
+
+        }
+    }
+}
