ce1ffb2d1d198f7cd1460fd131721887e771474d
daniel.li@artsolutions.io <daniel.li@artsolutions.io>
2024-05-10 09:52:10 +0800

    建置連線測試console

---
+++ ConsoleTools/ConnectionTest/ConnectionTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConnectionTest.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceClient service = new CrmServiceBuilder().ServiceClient;
+
+            Entity entityData = service.Retrieve("art_temptable", new Guid("8df11c3b-effa-ee11-9f89-00224859afce"), new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+            foreach(var attr in entityData.Attributes)
+            {
+                string info = attr.Key + "=" + attr.Value;
+                Console.WriteLine(info);
+            }
+
+            Console.WriteLine("Program Ends.");
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleTools/ConnectionTest/ConnectionTest/Services/CrmServiceBuilder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConnectionTest.Services
+{
+    internal class CrmServiceBuilder
+    {
+        public ServiceClient ServiceClient { get; set; }
+        public CrmServiceBuilder(string CallerId = "")
+        {
+            ServiceClient.MaxConnectionTimeout = new TimeSpan(1, 0, 0);
+            ServiceClient = GetServiceClient();
+            if (!string.IsNullOrEmpty(CallerId) && Guid.TryParse(CallerId, out Guid CallerGuid))
+            {
+                ServiceClient.CallerId = CallerGuid;
+            }
+        }
+        private ServiceClient GetServiceClient()
+        {
+            var builder = new ConfigurationBuilder().
+                SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            string appUrl = config["Url"];
+            string appClientId = config["ClientId"];
+            string appClientSecret = config["ClientSecret"];
+
+            string connectionString = $@"AuthType=ClientSecret;Url={appUrl};ClientId={appClientId};ClientSecret={appClientSecret}";
+            return new ServiceClient(connectionString);
+        }
+    }
+}
